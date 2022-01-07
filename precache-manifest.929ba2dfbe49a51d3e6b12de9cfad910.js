41782f77fe4e2000e6822fd983bfb1a72f6f3930
cheje <jcheng1@gradcenter.cuny.edu>
2022-01-06 21:34:40 -0500

    Updates

---
+++ precache-manifest.929ba2dfbe49a51d3e6b12de9cfad910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d46edd12d181d01702474a71da82cb1",
+    "url": "/csgc/index.html"
+  },
+  {
+    "revision": "1ea3d124edf75c13df95",
+    "url": "/csgc/static/css/main.4ee78b26.chunk.css"
+  },
+  {
+    "revision": "f01fb35f113420291869",
+    "url": "/csgc/static/js/2.188871c6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/csgc/static/js/2.188871c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea3d124edf75c13df95",
+    "url": "/csgc/static/js/main.760ec95d.chunk.js"
+  },
+  {
+    "revision": "eaf2005fe70176dd22ac",
+    "url": "/csgc/static/js/runtime-main.2cc73c6c.js"
+  }
+]);
